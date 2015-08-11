c50388f7fc6fadc9f581df3a58e9bf437d006b5b
ridchan <ridchan@ridchandeiMac.local>
2015-08-12 01:49:56 +0800

    hub

---
+++ angelslike/angelslike/RCHub.h
@@ -0,0 +1,20 @@
+//
+//  RCHub.h
+//  AMTumblrHudDemo
+//
+//  Created by ridchan on 15/8/11.
+//  Copyright (c) 2015年 askar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RoundHub : UIView
+    
+@end
+
+@interface RCHub : UIView
+
+-(void)startAnimation;
+-(void)stopAnimation;
+
+@end
